4557cb6b7c5d05579781f3337809866c6cc4d891
Ian M <ianmorrill03@gmail.com>
2017-07-24 21:13:32 -0400

    Add notifications

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 let username = ""
+let prevNotificationAmt = 0
 const init = () => {
   console.log("Starting...");
 
@@ -23,13 +24,28 @@ chrome.alarms.onAlarm.addListener(a => {
       console.log("Checking messages...");
       fetch(`https://api.scratch.mit.edu/proxy/users/${username}/activity/count`).then(r => r.json()).then(({msg_count}) => {
         if (msg_count > 0) {
+          if (msg_count > prevNotificationAmt) {
+            chrome.notifications.create({
+              type: "basic",
+              iconUrl: "icon128x128.png",
+              title: "New Scratch Message!",
+              message: "You now have " + msg_count + " message" + (msg_count === 1 ? "" : "s") + " on Scratch, click to view."
+            })
+          }
           chrome.browserAction.setBadgeText({text: msg_count + ""})
         }else{
           chrome.browserAction.setBadgeText({text: ""})
         }
+        prevNotificationAmt = msg_count
       })
     }
   }
+})
+
+chrome.notifications.onClicked.addListener(id => {
+  chrome.tabs.create({
+    url: "https://scratch.mit.edu/messages"
+  })
 })
 
 chrome.storage.onChanged.addListener((c, n) => {
