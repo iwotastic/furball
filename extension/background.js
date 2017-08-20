cc7966c0136628e95b7867e7b3cba1bf5e7368fd
Ian M <ianmorrill03@gmail.com>
2017-08-20 15:44:32 -0400

    Be small

--- extension/background.js
+++ extension/background.js
@@ -1,70 +1,8 @@
-let username = ""
-let notify = true
-let badge = true
-let prevNotificationAmt = 0
-const init = () => {
-  console.log("Starting...");
-
+chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.create("checkmessages", {
     periodInMinutes: 1
   })
 
-  console.log("Alarm created!");
-
-  chrome.storage.sync.get(["username", "notify"], (v) => { username = v.username || ""; notify = v.notify });
-
-  console.log("Username fetched!", username);
-}
-
-init()
-
-chrome.alarms.onAlarm.addListener(a => {
-  console.log("Alarm recieved.");
-  if (a.name === "checkmessages" && notify) {
-    console.log("Check recieved.");
-    if (username !== "") {
-      console.log("Checking messages...");
-      fetch(`https://api.scratch.mit.edu/proxy/users/${username}/activity/count`).then(r => r.json()).then(({msg_count}) => {
-        if (msg_count > 0) {
-          if (msg_count > prevNotificationAmt) {
-            chrome.notifications.create({
-              type: "basic",
-              iconUrl: "icon128x128.png",
-              title: "New Scratch Message" + (msg_count - prevNotificationAmt > 1 ? "s" : "") + "!",
-              message: "You now have " + msg_count + " message" + (msg_count === 1 ? "" : "s") + " on Scratch, click to view."
-            });
-          }
-          chrome.browserAction.setBadgeText({text: (badge ? msg_count : "") + ""});
-        }else{
-          chrome.browserAction.setBadgeText({text: ""});
-        }
-        prevNotificationAmt = msg_count;
-      });
-    }
-  }
-});
-
-chrome.notifications.onClicked.addListener(id => {
-  chrome.tabs.create({
-    url: "https://scratch.mit.edu/messages"
-  });
-});
-
-chrome.storage.onChanged.addListener((c, n) => {
-  if (n === "sync") {
-    username = (c.username || {newValue: username}).newValue;
-    notify = (c.notify || {newValue: notify}).newValue;
-    badge = (c.badge || {newValue: badge}).newValue;
-  }
-});
-
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.create({
-    url: "https://scratch.mit.edu/messages"
-  });
-});
-
-chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.get(["fixedNavbar", "notify", "bbDiscuss", "bbwiki", "badge", "searchEngine"], (v) => {
     if (v["fixedNavbar"] == null || v["fixedNavbar"] == undefined) {
       chrome.storage.sync.set({fixedNavbar: true}, () => {});
@@ -84,5 +22,54 @@ chrome.runtime.onInstalled.addListener(() => {
     if (v["searchEngine"] == null || v["searchEngine"] == undefined) {
       chrome.storage.sync.set({searchEngine: "google"}, () => {});
     }
+  });
+});
+
+chrome.alarms.onAlarm.addListener(a => {
+  console.log("Alarm recieved.");
+  chrome.storage.sync.get(["username", "notify", "prevNotificationAmt"], ({username, notify, prevNotificationAmt}) => {
+    if (a.name === "checkmessages" && notify) {
+      console.log("Check recieved.");
+      if (username !== "") {
+        console.log("Checking messages...");
+        fetch(`https://api.scratch.mit.edu/proxy/users/${username}/activity/count`).then(r => r.json()).then(({msg_count}) => {
+          console.log("Diff n, o, in", msg_count, prevNotificationAmt, msg_count > prevNotificationAmt);
+          if (msg_count > 0) {
+            if (msg_count > prevNotificationAmt) {
+              chrome.notifications.create({
+                type: "basic",
+                iconUrl: "icon128x128.png",
+                title: "New Scratch Message" + (msg_count - prevNotificationAmt > 1 ? "s" : "") + "!",
+                message: "You now have " + msg_count + " message" + (msg_count === 1 ? "" : "s") + " on Scratch, click to view."
+              });
+            }
+            chrome.browserAction.setBadgeText({text: (badge ? msg_count : "") + ""});
+          }else{
+            chrome.browserAction.setBadgeText({text: ""});
+          }
+          chrome.storage.sync.set({prevNotificationAmt: msg_count}, () => {});
+        });
+      }
+    }
+  });
+});
+
+chrome.notifications.onClicked.addListener(id => {
+  chrome.tabs.create({
+    url: "https://scratch.mit.edu/messages"
+  });
+});
+
+chrome.storage.onChanged.addListener((c, n) => {
+  if (n === "sync") {
+    username = (c.username || {newValue: username}).newValue;
+    notify = (c.notify || {newValue: notify}).newValue;
+    badge = (c.badge || {newValue: badge}).newValue;
+  }
+});
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.tabs.create({
+    url: "https://scratch.mit.edu/messages"
   });
 });
